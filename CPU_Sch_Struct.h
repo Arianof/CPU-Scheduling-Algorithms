c2aaffed66e90ef0c2e24d57ffba6f5b1fdfe9d4
Arianof <Arian.fathi03@gmai.com>
2024-03-27 12:54:05 +0430

    feat: Cpu_Sch struct has been developed for easier implementaion of scheduling algorithms

---
+++ CPU_Sch_Struct.h
@@ -0,0 +1,8 @@
+#define default_amount 50
+struct CPU_Sdl{
+    int pid[default_amount];
+    int priority[default_amount];
+    int bt[default_amount];
+    int wt[default_amount];
+    int TurnaroundTime[default_amount];
+};
